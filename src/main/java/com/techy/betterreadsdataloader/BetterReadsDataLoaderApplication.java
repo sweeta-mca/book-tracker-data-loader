b39aa89e1edb2b9fab44d4f56f750277117a7183
unknown <sweetamca@gmail.com>
2021-10-04 22:44:10 +0400

    setting up connection with Cassandra

---
+++ src/main/java/com/techy/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -0,0 +1,27 @@
+package com.techy.betterreadsdataloader;
+
+import java.nio.file.Path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import connection.DataStaxAstraProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
+public class BetterReadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
+	}
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties)
+	{
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+}
---
+++ src/main/java/connection/DataStaxAstraProperties.java
@@ -0,0 +1,20 @@
+package connection;
+
+import java.io.File;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "datastax.astra")
+public class DataStaxAstraProperties {
+    private File SecureConnectBundle;
+
+    public File getSecureConnectBundle() {
+        return SecureConnectBundle;
+    }
+
+    public void setSecureConnectBundle(File secureConnectBundle) {
+        SecureConnectBundle = secureConnectBundle;
+    }
+
+    
+}
